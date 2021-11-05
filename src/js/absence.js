5d6ee3699bd37fe413150540a8e75e082b2a61c9
RABOT Mathieu <mathieu.rabot@cpnv.ch>
2021-11-05 15:01:30 +0100

    creating modal window

--- src/js/absence.js
+++ src/js/absence.js
@@ -1,3 +1,16 @@
+const open = document.getElementById('open');
+const modal = document.getElementById('modal-container');
+
+if(open){
+  open.addEventListener('click',() => {
+    modal.classList.add('show');
+  });
+}
+
+function closeModale(el) {
+  modal.classList.remove('show');
+}
+
 function changeColor(el) {
   if (el.style.backgroundColor === "red") {
     el.style.backgroundColor = "#4CB770FF";
