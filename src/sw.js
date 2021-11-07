366972e3a1232e5f6f900d673bdbb6e46708ce8c
maurxsantoz <maurx18@gmail.com>
2021-11-08 00:48:43 +0100

    last version before testing

--- src/sw.js
+++ src/sw.js
@@ -1,40 +1,34 @@
-var cacheName = 'hello-pwa'
+const cacheName = 'intranet';
 
-var filesToCache = [
-
-  '/',
-
-  '/index.html',
-
-  '/css/style.css',
-
-
-]
-
-/* Start the service worker and cache all of the app's content */
-
-self.addEventListener('install', function (e) {
-
+// Cache all the files to make a PWA
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(cache => {
+      // Our application only has two files here index.html and manifest.json
+      // but you can add more such as style.css as your app grows
+      return cache.addAll([   
+        'js/*',
+        'index.html',
+        'index.php',
+        'manifest.json',
+        'images/*',
+        'ressource/*',
+        'css/*',
+        'html/*',
+      ]);
+    })
+  );
+});
 
-      return cache.addAll(filesToCache)
-
-    }),
-  )
-
-})
-
-/* Serve cached content when offline */
-
-self.addEventListener('fetch', function (e) {
-
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-
-      return response || fetch(e.request)
-
-    }),
-  )
-
-})
+// Our service worker will intercept all fetch requests
+// and check if we have cached the file
+// if so it will serve the cached file
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
